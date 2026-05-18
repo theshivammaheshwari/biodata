c6849a840a5b4ac0e121111cff03ff17a71bfee7
Shivam Maheshwari <theshivammaheshwari@gmail.com>
2026-05-18 23:56:05 +0530

    cd4

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/biodata/',
+  base: process.env.VERCEL ? '/' : '/biodata/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
